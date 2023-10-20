51fafbfdcd9fdd7cdbdb5aeb75d82b056c573749
Josh Pirog <52870102+joshpirog@users.noreply.github.com>
2023-10-20 15:55:41 -0400

    Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.values(description).map((element, index) => {
+        return <p className="tile" key={index}>{element}</p>
+      })}
     </div>
   );
 };
